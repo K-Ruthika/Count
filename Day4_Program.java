88a1cb2b8097e75942c88d58878dae9df6f2ff9a
Ruthika k <160602674+K-Ruthika@users.noreply.github.com>
2025-03-07 10:34:59 +0530

    Update Day4_Program.java

--- Day4_Program.java
+++ Day4_Program.java
@@ -2,7 +2,27 @@
 public class Day4_Program {
 
 	public static void main(String[] args) {
-		
+		for(int i=0; i<n; i++)
+		 {
+			 a[i] = sc.nextInt();
+		 }
+		 boolean visited[] = new boolean[n];
+		 for(int i=0; i<n; i++)
+		 {
+			 if(visited[i]==true)
+			 {
+				 continue;
+			 }
+			 int count=1;
+			 for(int j=i+1; j<n; j++)
+			 {
+				 if(a[i] == a[j])
+				 {
+					 visited[j] = true;
+					 count++;
+				 }
+			 }
+			 System.out.println(a[i]+ " " +count);
 
 	}
 
